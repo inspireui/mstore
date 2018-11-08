f24c4fa3ae1fafd90130b67038a2a500e318f823
thangvo9x <thangvo5969@gmail.com>
2018-11-08 17:28:07 +0700

    Update posts.php
    update json support post comment, fb...

--- plugins/json-api/controllers/posts.php
+++ plugins/json-api/controllers/posts.php
@@ -8,20 +8,30 @@ class JSON_API_Posts_Controller {
 
   public function create_post() {
     global $json_api;
-    if (!current_user_can('edit_posts')) {
-      $json_api->error("You need to login with a user that has 'edit_posts' capacity.", 403);
-    }
+    if (!$json_api->query->cookie) {
+		$json_api->error("You must include a ‘cookie’ var in your request. Use the generate_auth_cookie Auth API method.");
+	}
     if (!$json_api->query->nonce) {
       $json_api->error("You must include a 'nonce' value to create posts. Use the `get_nonce` Core API method.", 403);
     }
+
+	if (!$json_api->query->author) {
+		$json_api->error("You must include ‘author’ var in your request.");
+	}
+	// if (!$json_api->query->parent) {
+	//   $json_api->error("You must include ‘parent’ var in your request.");
+	// }
+
     $nonce_id = $json_api->get_nonce_id('posts', 'create_post');
     if (!wp_verify_nonce($json_api->query->nonce, $nonce_id)) {
       $json_api->error("Your 'nonce' value was incorrect. Use the 'get_nonce' API method.", 403);
     }
     nocache_headers();
     $post = new JSON_API_Post();
-    $id = $post->create($_REQUEST);
-    if (empty($id)) {
+	$post->set_author_value($json_api->query->author);
+	$id = $post->create($_REQUEST);
+
+    if (empty($id)) { 
       $json_api->error("Could not create post.", 500);
     }
     return array(
